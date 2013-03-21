400cc9cb6f1d67a80312e47638d1a4cf52987e2d
Joao <Joao@Cristina-PC>
2013-03-21 16:27:31 +0000

    Adicionei as classes:IncomeTypeUI, IncomeTypeRepository ,IncomeTypesControler, IncomeTypes

---
+++ eapli.expensemanager/src/Persistence/IncomeTypeRepository.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Persistence;
+
+/**
+ *
+ * @author Joao
+ */
+public class IncomeTypeRepository {
+    
+}
