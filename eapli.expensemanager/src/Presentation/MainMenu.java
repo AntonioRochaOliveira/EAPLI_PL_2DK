ed9e486a8f588ba3462d406d06485565a5aae367
tiago fernandes <tiago_andre_fernandes@hotmail.com>
2013-03-21 16:13:37 +0000

    alteração do menu

--- eapli.expensemanager/src/Presentation/MainMenu.java
+++ eapli.expensemanager/src/Presentation/MainMenu.java
@@ -16,18 +16,51 @@ public class MainMenu {
         System.out.println("  EXPENSE MANAGER  ");
         System.out.println("===================\n");
         
-        System.out.println("1. Register an expense");
-        System.out.println("0. Exit\n\n");
+        System.out.println("1. Registar tipos de despesa");
+        System.out.println("2. Definir tipos de pagamento");
+        System.out.println("3. Registar despesa");
+        System.out.println("4. Registo de entrada de rendimentos");
+        System.out.println("5. Definição de tipos de rendimento");
+        System.out.println("6. Inicialização do saldo");
+        System.out.println("7. Visualização de gasto da semana");
+        System.out.println("8. Consulta de gastos mensais");
         
-        int option = Console.readInteger("Please choose a option");
+        System.out.println("0. Sair\n\n");
+        
+        
+        int option = Console.readInteger("Escolhe uma opcao");
+        
         switch (option) {
             case 0: 
-                System.out.println("bye bye ...");
-                return;
+                System.out.println("Adeus");
+                break;
             case 1: 
+                //regista tipo de despesa
+                break;
+            case 2:
+                //define tipos de pagamento
+                break;
+                 case 3: 
                 ExpenseRegisterUI ui = new ExpenseRegisterUI();
                 ui.mainLoop();
                 break;
+                case 4: 
+                //Registo de entrada de rendimentos
+                break;
+                case 5: 
+                //DEf de tipos de rendimento
+                break;
+                case 6: 
+                //inicializa saldo
+                break;
+                case 7: 
+                //visualizar gasto da semana
+                break;
+                case 8: 
+                //consulta de gasto mensal
+                break;
+                default:
+                    break;           
         }
     }
 }
