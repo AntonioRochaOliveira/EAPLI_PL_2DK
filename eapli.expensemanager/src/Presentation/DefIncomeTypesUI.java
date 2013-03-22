cf8709b893288872b7337db8677114be5f12ae6a
i110415 <i110415@PCI027.dei.isep.ipp.pt>
2013-03-22 14:35:13 +0000

    Implementação do mainLoop para adicionar novos tipos de rendimento; Mostra a lista de rendimentos já existentes, caso existam;

--- eapli.expensemanager/src/Presentation/DefIncomeTypesUI.java
+++ eapli.expensemanager/src/Presentation/DefIncomeTypesUI.java
@@ -1,21 +1,27 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package Presentation;
 
-/**
- *
- * @author Joao
- */
-import java.awt.*;
-import java.awt.event.*;
-import javax.swing.*;
-import javax.swing.border.*;
+import eapli.util.Console;
+import java.util.ArrayList;
 
 import Controllers.IncomeTypeController;
 
 public class DefIncomeTypesUI {
-
     IncomeTypeController IncTC = new IncomeTypeController();
+    
+    public void mainLoop(){
+        System.out.println( "* * *  NOVO TIPO DE RENDIMENTO  * * *\n" );
+        
+        ArrayList lista = IncTC.getLista();
+        if ( lista.size() != 0 ){
+            System.out.println( "Tipos de rendimentos já existentes:" );
+            for ( int a = 0 ; a < lista.size() ; a++ ){
+                System.out.println( " - " + lista.get(a).toString() + ";");
+            }
+        }
+    }
+    
+    public static void main( String[] args ){
+        DefIncomeTypesUI test = new DefIncomeTypesUI();
+        test.mainLoop();
+    }
 }
