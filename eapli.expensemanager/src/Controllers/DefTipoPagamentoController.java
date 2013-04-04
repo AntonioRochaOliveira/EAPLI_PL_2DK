7d72ecbd1742d298bebcb09ccef00da420048429
Fernando Pinto <nandinhuh_10@hotmail.com>
2013-04-04 18:42:26 +0100

    DefTipoPagamentoController implementada

--- eapli.expensemanager/src/Controllers/DefTipoPagamentoController.java
+++ eapli.expensemanager/src/Controllers/DefTipoPagamentoController.java
@@ -4,12 +4,16 @@ import Model.TipoPagamento;
 import Persistence.RepositorioTiposPagamento;
 
 public class DefTipoPagamentoController {
+    private RepositorioTiposPagamento rep;
     
     public DefTipoPagamentoController(){}
     
     public void NovoTipoPagamento(String descricao){
         TipoPagamento tipo_pagamento = new TipoPagamento(descricao);
-        RepositorioTiposPagamento rep = new RepositorioTiposPagamento();
         rep.AdicionarTipoPagamento(tipo_pagamento);
+    }
+    
+    public RepositorioTiposPagamento getRepositorioTiposPagamento(){
+        return rep;
     }
 }
