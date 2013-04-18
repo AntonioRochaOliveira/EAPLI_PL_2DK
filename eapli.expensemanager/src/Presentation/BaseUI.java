56d1fd6acc37b7b2dcee3eba60ec651301d28f60
Joao <Joao@Cristina-PC>
2013-04-18 15:52:17 +0100

    Alterei a BaseUI para que o metodo construtor nao diga logo o saldo mas sim o novo metodo displaySaldo

--- eapli.expensemanager/src/Presentation/BaseUI.java
+++ eapli.expensemanager/src/Presentation/BaseUI.java
@@ -3,15 +3,21 @@
  * and open the template in the editor.
  */
 package Presentation;
+
 import Controllers.BaseController;
+
 /**
  *
  * @author Joao
  */
 public class BaseUI {
-    BaseController BaseContr=new BaseController();
-    public BaseUI(){
-        System.out.println("Saldo corrente: "+ BaseContr.getSaldo());
-        
+
+    BaseController BaseContr = new BaseController();
+
+    public BaseUI() {
+    }
+
+    public void displaySaldo() {
+        System.out.println("Saldo corrente: " + BaseContr.getSaldo());
     }
 }
