ec92072be8eb1cd97fa69b86143b77ad99dfd713
paulo <paulo@paulomonteiro>
2013-04-26 13:59:12 +0100

    alteracao da classe InicializacaoSaldoUI , pois era necessario a reformulação da mesma e o grupo responsável nao estava presente.
    Paulo Monteiro 1110255

--- eapli.expensemanager/src/Presentation/InicializacaoSaldoUI.java
+++ eapli.expensemanager/src/Presentation/InicializacaoSaldoUI.java
@@ -12,14 +12,19 @@ import eapli.util.Console;
 import java.math.BigDecimal;
 import java.util.Date;
 
+/*alteracao da classe InicializacaoSaldoUI , pois era necessario a reformulação da mesma 
+ * e o grupo responsável nao estava presente.
+ * 
+ * Paulo Monteiro 1110255
+*/
+
 public class InicializacaoSaldoUI extends BaseUI{
     
-    IncomeRepository irp;
+     IncomeRepository irp;
+     private String title = "Inicializar Saldo";
+     private InitSaldoController controlador = new InitSaldoController();
     
-    public void mainLoop() {
-        
-        System.out.println("* * *  Inicializar Saldo  * * *\n");
-        displaySaldo();
+    public void showBody() {
         
         int valorS = Console.readInteger("Inserir valor:");
         BigDecimal amount = new BigDecimal(valorS);
@@ -27,8 +32,22 @@ public class InicializacaoSaldoUI extends BaseUI{
         String descricao = Console.readLine("Descricao:");  
         IncomeTypes tipoS = new IncomeTypes("Saldo");
         
-        InitSaldoController controladorSaldo = new InitSaldoController();
-        controladorSaldo.inicializar(amount, data, descricao, tipoS);
+        
+        controlador.inicializar(amount, data, descricao, tipoS);
         System.out.println("Saldo inicializado com o valor: "+valorS+".\n");
+    }
+    
+    /**
+     * @return the title
+     */
+    public String getTitle() {
+        return title;
+    }
+
+    /**
+     * @return the controller
+     */
+    public BaseController getController() {
+        return controlador;
     }
 }
