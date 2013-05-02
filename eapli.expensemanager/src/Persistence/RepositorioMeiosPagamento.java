864436f2b541eebe1f0f146ba21610c62c0017c4
Daniel João Leal Ferreira <1080633@isep.ipp.pt>
2013-05-02 16:47:14 +0100

    classes InMemory MeiosPagamento

--- eapli.expensemanager/src/Persistence/RepositorioMeiosPagamento.java
+++ eapli.expensemanager/src/Persistence/RepositorioMeiosPagamento.java
@@ -3,42 +3,10 @@ package Persistence;
 import Model.MeioPagamento;
 import java.util.ArrayList;
 
-public class RepositorioMeiosPagamento {
+public interface RepositorioMeiosPagamento {
     
-    private static RepositorioMeiosPagamento uniqueInstance = null;
-    private static ArrayList<MeioPagamento> lista_meiosPagamento = new ArrayList<MeioPagamento>();
-    
-    private RepositorioMeiosPagamento(){}
-    
-    public void AdicionarMeioPagamento(MeioPagamento meio_pag){
-        
-        if (meio_pag==null){
-            throw new IllegalArgumentException();
-        }
-        lista_meiosPagamento.add(meio_pag);
-    }
-    
-    
-
-    public ArrayList<MeioPagamento> getLista_meiosPagamento() {
-        return lista_meiosPagamento;
-    }
-    
-    public static RepositorioMeiosPagamento instance(){
-        if(uniqueInstance==null){
-            uniqueInstance = new RepositorioMeiosPagamento();
-        }
-        return uniqueInstance;
-    }
-    
-    public MeioPagamento getMeioPagamento(String descricao){
-        MeioPagamento tmp = null;
-        for(MeioPagamento m : lista_meiosPagamento){
-            if(m.getDescricao().compareTo(descricao)==0){
-                tmp=m;
-            }
-        }
-        return tmp;
-    }
+    public void AdicionarMeioPagamento(MeioPagamento meio_pag);
+    public ArrayList<MeioPagamento> getLista_meiosPagamento();   
+    public MeioPagamento getMeioPagamento(String descricao);
         
 }
