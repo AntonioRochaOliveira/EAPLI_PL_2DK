3ab4b02eb1e048618af3bffab53d29bf668cafc4
Antonio <1070352@isep.ipp.pt>
2013-05-02 15:35:50 +0100

    BaseUI integrado na classe registo de rendimentos

--- eapli.expensemanager/src/Controllers/IncomeRegisterController.java
+++ eapli.expensemanager/src/Controllers/IncomeRegisterController.java
@@ -12,7 +12,7 @@ import java.math.BigDecimal;
 
 //Eu Pedro Passo alterei esta classe para ter acesso a alguns metodos que nao estava a conseguir
 //nomeadamente aceder ao repositorio
-public class IncomeRegisterController {
+public class IncomeRegisterController extends BaseController{
     IncomeRepository inco;
     
     
