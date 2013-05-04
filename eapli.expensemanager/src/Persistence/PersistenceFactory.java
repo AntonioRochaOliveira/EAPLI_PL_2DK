5d14364c3fda9e01268267efc64a33e5f1017508
Pedro Nogueira <Pedro Nogueira@PedroNogueira.lan>
2013-05-04 15:52:35 +0100

    persistenceFactory.java
    instanciado por reflaction mediante um ficheiro existente no disco "config.proprities"

--- eapli.expensemanager/src/Persistence/PersistenceFactory.java
+++ eapli.expensemanager/src/Persistence/PersistenceFactory.java
@@ -4,6 +4,8 @@
  */
 package Persistence;
 
+import java.io.FileInputStream;
+import java.util.Properties;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -11,15 +13,28 @@ import java.util.logging.Logger;
  *
  * @author arocha
  */
+
 public class PersistenceFactory {
-    
     private PersistenceFactory() {
     }
 
     public static RepositoryFactory buildPersistenceFactory() {
-    //JpaRepositoryFactory repo = new JpaRepositoryFactory();
-    InMemoryRepositoryFactory repo= new InMemoryRepositoryFactory();
-        return (RepositoryFactory)  repo;
-  
+    	return loadRepositoryFactoryFromFile();
     }
+
+	private static RepositoryFactory loadRepositoryFactoryFromFile() {
+		Properties persistence = new Properties();
+		String PROPERTIES_PATH = "config/config.properties";
+		try{
+			persistence.load(new FileInputStream(PROPERTIES_PATH));
+			String property = PersistenceFactory.class.getSimpleName() + ".useFactory";
+			Class factoryClass = Class.forName(persistence.getProperty(property));
+			return (RepositoryFactory) factoryClass.newInstance();
+		}catch(Exception e){
+			System.err.println("Unable to load persistence factory");
+			e.printStackTrace(System.err);
+			System.err.println("Reverting to Memory");
+			return new InMemoryRepositoryFactory();
+		}
+	}
 }
