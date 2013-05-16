ecfafc102039dbceb3dba9a8d7221029e58f95ca
Joao <Joao@Cristina-PC>
2013-05-16 15:49:13 +0100

    alterei a incomeType para colocar no repositorio

--- eapli.expensemanager/src/Model/IncomeTypes.java
+++ eapli.expensemanager/src/Model/IncomeTypes.java
@@ -1,20 +1,36 @@
 package Model;
 
-public class IncomeTypes {
+import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class IncomeTypes implements Serializable {
+
+    @Id
+    private int pk = 0;
     private String IncomeType;
 
-     public IncomeTypes(String IncomeType){
-         this.IncomeType=IncomeType;
-     }
-            
+    public IncomeTypes() {
+    }
+
+    public IncomeTypes(String IncomeType, int pk) {
+        if (IncomeType == null || IncomeType == "" || pk == 0 ) {
+            throw new IllegalArgumentException();
+        }
+        this.IncomeType = IncomeType;
+        this.pk=pk;
+    }
+
     public String getIncomeType() {
         return IncomeType;
     }
+
     public void setIncomeType(String IncomeType) {
         this.IncomeType = IncomeType;
     }
-    
-    public String toString(){
+
+    public String toString() {
         return IncomeType;
     }
 }
